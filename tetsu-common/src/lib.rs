ed01344af4d4f859fcbbd9fa6d76727000043375
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2026-03-19 12:18:40 +0530

    feat: add the load balacer logic

--- tetsu-common/src/lib.rs
+++ tetsu-common/src/lib.rs
@@ -1,1 +1,12 @@
 #![no_std]
+
+use aya_ebpf_bindings::bindings::ETH_ALEN;
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct Backend {
+    pub mac: [u8; ETH_ALEN as usize],
+}
+
+unsafe impl aya_ebpf::cty::Send for Backend {}
+unsafe impl aya_ebpf::cty::Sync for Backend {}
--- tetsu-ebpf/src/main.rs
+++ tetsu-ebpf/src/main.rs
@@ -1,52 +1,88 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_ebpf::{
+    bindings::xdp_action,
+    macros::{map, xdp},
+    programs::XdpContext,
+    EbpfContext,
+};
 use aya_log_ebpf::info;
+use core::mem;
+use tetsu_common::Backend;
 
-static PACKET_COUNT: HashMap<u8, u64> = HashMap::with_max_entries(256, 0);
+// This Map stores our Backend Servers.
+// Key: The VIP (Virtual IP) address (u32).
+// Value: The Backend's MAC address.
+#[map]
+static mut BACKENDS: HashMap<u32, Backend> = HashMap::with_max_entries(1024, 0);
 
 #[xdp]
-pub fn packet_counter(ctx: XdpContext) -> u32 {
-    match try_packet_counter(&ctx){
-        Ok(action) => action,
-        Err(_) => xdp_action::XDP_PASS,
+pub fn tetsu_lb(ctx: XdpContext) -> u32 {
+    match try_tetsu_lb(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_PASS, // If we fail, let the kernel handle it
     }
 }
 
-fn try_packet_counter(ctx: &XdpContext) -> Result<u32, ()>{
-    let data = ctx.data();
-    let data_end = ctx.data_end();
+fn try_tetsu_lb(ctx: XdpContext) -> Result<u32, u32> {
+    let offset = ctx.data();
+    let end = ctx.data_end();
 
-    let eth_header_size = 14;
-    
-    // requires explicit bounds checking before memory access
-    if data + eth_header_size > data_end{
-        return Ok(xdp_action::XDP_PASS);
+    let eth_hdr: *mut EthHdr = unsafe { ptr_at(offset, 0)? };
+
+    unsafe {
+        if (*eth_hdr).proto != 0x08_u16 && (*eth_hdr).proto != 0x00_u16 {
+        }
     }
 
-    let ethertype_ptr = (data + 12) as *count u16;
-    let ethertype = unsafe { *ethertype_ptr};
+    let ip_hdr_offset = offset + 14;
+    let ip_hdr: *mut IpHdr = unsafe { ptr_at(ip_hdr_offset, 0)? };
 
-    if ethertype != 0x0008u16{
-        return Ok(xdp_action::XDP_PASS);
+    unsafe {
+        if (*ip_hdr).protocol != 6 {
+            return Ok(xdp_action::XDP_PASS);
+        }
+
+        let vip = u32::from_be((*ip_hdr).dst_addr);
+        
+        if let Some(backend) = BACKENDS.get(&vip) {
+            (*eth_hdr).dst = backend.mac;
+            
+            (*eth_hdr).src = (*eth_hdr).src; // Placeholder: usually stays as is or updates to LB MAC
+
+            info!(&ctx, "Forwarding packet to backend");
+            return Ok(xdp_action::XDP_TX);
+        }
     }
 
-    let protocol_ptr = (ip_header_start + protocol_offset) as *count u8;
-    let protocol = unsafe{*protocol_ptr};
-
-    info!(ctx, "Recieved ipv4 packet with protocol:{}", protocol);
-
-    if let Some(count) = unsafe {PACKET_COUNT.get_ptr_mut(&protocol)}{
-        unsafe {*count += 1};
-    }
-    else {
-        let _ = PACKET_COUNT.insert(&protocol, &1, 0);
-    }
     Ok(xdp_action::XDP_PASS)
 }
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> !{
-    loop{}
+#[repr(C)]
+struct EthHdr {
+    dst: [u8; 6],
+    src: [u8; 6],
+    proto: u16,
+}
+
+#[repr(C)]
+struct IpHdr {
+    _tos: u8,
+    _len: u16,
+    _id: u16,
+    _flags: u16,
+    ttl: u8,
+    protocol: u8,
+    _checksum: u16,
+    src_addr: u32,
+    dst_addr: u32,
+}
+
+unsafe fn ptr_at<T>(offset: usize, off: usize) -> Result<*mut T, u32> {
+    let ptr = (offset + off) as *const T;
+    if offset as usize + off + mem::size_of::<T>() > ctx.data_end() as usize {
+        return Err(1);
+    }
+    Ok(ptr as *mut T)
 }
